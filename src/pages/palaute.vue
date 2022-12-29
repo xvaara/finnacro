7c2679ee2794145abd0fb013ee9b511f2e5f5bbf
Jukka Raimovaara <roska@mentalhouse.fi>
2022-12-29 23:31:35 +0200

    info symlink, palaute rericet

---
+++ src/pages/palaute.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const url = 'https://google.com'
+window.location.replace(url)
+</script>
+
+<template>
+  <div btn text-sm m="3 t8">
+    <a :href="url">
+      Siirry palautteeseen
+    </a>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
