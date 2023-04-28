1aa8d91f36badcfa8cb71830a7d92f19c468e2ae
xvaara <xvaara@users.noreply.github.com>
2023-04-28 11:41:14 +0000

    deploy: 7132dd76cde58bbe13aa655b537ecde37d04758b

--- assets/home-ae23216f.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,r,c,a as e,o as s,f as a}from"./app-a1d4b00a.js";const _={},m={class:"px-4 py-10 text-center text-gray-700 dark:text-gray-200"};function i(p,f){const o=r("RouterView"),n=a;return s(),c("main",m,[e(o),e(n)])}const l=t(_,[["render",i],["__file","/home/runner/work/finnacro/finnacro/src/layouts/home.vue"]]);export{l as default};
